98e804e28ad14d8b93ae77eac26728b48fc73b49
Daniel Durán <107888507+Charmandiox9@users.noreply.github.com>
2025-09-08 23:06:21 -0300

    Update ejercicio1.cpp

--- Ayudantía 3/Solve/ejercicio1.cpp
+++ Ayudantía 3/Solve/ejercicio1.cpp
@@ -18,31 +18,27 @@ class Solution {
         Node* current = head;
         
         while(current != nullptr) {
-            // Avanzar m-1 posiciones (ya estamos en la primera)
             for(int i = 1; i < m && current != nullptr; i++) {
                 current = current->next;
             }
             
-            // Si current es nullptr, hemos llegado al final
             if(current == nullptr) {
                 break;
             }
             
-            // Guardar el nodo desde donde vamos a eliminar
             Node* nodeToDelete = current->next;
             
-            // Eliminar los siguientes n nodos
             for(int i = 0; i < n && nodeToDelete != nullptr; i++) {
                 Node* temp = nodeToDelete;
                 nodeToDelete = nodeToDelete->next;
                 delete temp;
             }
             
-            // Conectar el último nodo mantenido con el siguiente grupo
             current->next = nodeToDelete;
             current = nodeToDelete;
         }
         
         return head;
     }
+
 };
