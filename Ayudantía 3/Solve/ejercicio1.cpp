1d430a9a7934bf3863e79d92dc95f45293a68b54
Daniel Durán <107888507+Charmandiox9@users.noreply.github.com>
2025-09-08 23:07:57 -0300

    Update ejercicio1.cpp

--- Ayudantía 3/Solve/ejercicio1.cpp
+++ Ayudantía 3/Solve/ejercicio1.cpp
@@ -8,37 +8,35 @@ struct Node {
     }
 };
 
-class Solution {
-  public:
-    Node* linkdelete(Node* head, int n, int m) {
-        if(head == nullptr || m == 0) {
-            return nullptr;
-        }
-        
-        Node* current = head;
-        
-        while(current != nullptr) {
-            for(int i = 1; i < m && current != nullptr; i++) {
-                current = current->next;
-            }
-            
-            if(current == nullptr) {
-                break;
-            }
-            
-            Node* nodeToDelete = current->next;
-            
-            for(int i = 0; i < n && nodeToDelete != nullptr; i++) {
-                Node* temp = nodeToDelete;
-                nodeToDelete = nodeToDelete->next;
-                delete temp;
-            }
-            
-            current->next = nodeToDelete;
-            current = nodeToDelete;
-        }
-        
-        return head;
-    }
 
-};
+Node* linkdelete(Node* head, int n, int m) {
+    if(head == nullptr || m == 0) {
+        return nullptr;
+    }
+    
+    Node* current = head;
+    
+    while(current != nullptr) {
+        for(int i = 1; i < m && current != nullptr; i++) {
+            current = current->next;
+        }
+        
+        if(current == nullptr) {
+            break;
+        }
+        
+        Node* nodeToDelete = current->next;
+        
+        for(int i = 0; i < n && nodeToDelete != nullptr; i++) {
+            Node* temp = nodeToDelete;
+            nodeToDelete = nodeToDelete->next;
+            delete temp;
+        }
+        
+        current->next = nodeToDelete;
+        current = nodeToDelete;
+    }
+    
+    return head;
+}
+
